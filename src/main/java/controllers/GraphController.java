9d93e1f23f7268da5c39ad95c50c4603ef779204
James <jschalle@oswego.edu>
2018-10-07 06:03:35 -0400

    fixed daily average prices

--- src/main/java/controllers/GraphController.java
+++ src/main/java/controllers/GraphController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class GraphController {
@@ -11,6 +12,13 @@ public class GraphController {
     public static XYChart.Series getGraphData(JSONArray array, int totalNum){
         XYChart.Series series = new XYChart.Series();
         int spacer = array.length() / totalNum;
+        String average;
+        try{
+            ((JSONObject)array.get(0)).get("vwap");
+            average = "vwap";
+        }catch (JSONException e){
+            average = "average";
+        }
         for(int i = 0; i < totalNum; i+=spacer){
             if(i  == totalNum -1){
                 series.getData().add(new XYChart.Data(((JSONObject)array.get(i)).get("date"), ((JSONObject)array.get(i)).get("vwap")));
