e7a629116f918565e687080fce9e84e7af176cbf
Landon Patmore <landon.patmore@gmail.com>
2018-10-14 22:45:56 -0400

    Sentiment analysis update
    - removed article sentiment analysis class as it was not needed
    - added article sentiment analysis methods to sentiment analyzer
    - fixed issue with article sentiment score being the same for all
    articles
    - cleaned up code

--- src/main/java/dataworkers/SentimentAnalyzer.java
+++ src/main/java/dataworkers/SentimentAnalyzer.java
@@ -1,7 +1,6 @@
 package dataworkers;
 
 import dataobjects.Article;
-import dataobjects.ArticleSentiment;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
@@ -10,39 +9,45 @@ import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
+import model.Log;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Properties;
 
 public class SentimentAnalyzer {
+    private static final Log logger = new Log(SentimentAnalyzer.class);
 
     private static int[] articleScore = new int[5];
 
     public static void getSentimentScore(Article a) {
+        logger.info("Starting sentiment analysis on article: " + a.getTitle(), false);
 
         String[] sentences = getSentences(a);
 
         int sentenceScore;
 
-        for(String s : sentences){
+        for (String s : sentences) {
             sentenceScore = analyzeSentiment(s);
-            if(sentenceScore > 0)
+            if (sentenceScore > 0)
                 articleScore[sentenceScore]++;
         }
 
-        ArticleSentiment sentiment = new ArticleSentiment(articleScore);
+        a.setSentiment(getSentiment(articleScore));
 
-        a.setSentiment(sentiment);
+        logger.info("Sentiment: " + a.getSentiment(), false);
+
+        logger.info("Finished sentiment analysis on article: " + a.getTitle(), false);
     }
 
     private static int analyzeSentiment(String sentence) {
 
-        Properties props = new Properties();
+        final Properties props = new Properties();
         props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        final StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
-        Annotation annotation = pipeline.process(sentence);
-        List<CoreMap> cm = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+        final Annotation annotation = pipeline.process(sentence);
+        final List<CoreMap> cm = annotation.get(CoreAnnotations.SentencesAnnotation.class);
 
         int sentimentScore = -1;
 
@@ -58,15 +63,36 @@ public class SentimentAnalyzer {
 
         String text = "";
 
-        if(a.getBody() != null)
+        if (a.getBody() != null)
             text = a.getBody().text();
 
         text = text.replace(". ", ".~");
         text = text.replace("? ", ".~");
         text = text.replace("! ", ".~");
 
-        String[] sentences = text.split("~");
+        return text.split("~");
+    }
 
-        return sentences;
+    private static String getSentiment(int[] scores) {
+        final DecimalFormat df = new DecimalFormat("0.##");
+        final int overallSentiment = scores[4] * 2 + scores[3] - scores[1] - scores[0] * 2;
+        final int sentencesAnalyzed = scores[4] + scores[3] + scores[2] + scores[1] + scores[0];
+        double sentimentPct;
+        boolean bullish;
+
+        if (sentencesAnalyzed > 0) {
+            sentimentPct = ((double) overallSentiment / sentencesAnalyzed) * 100;
+        } else {
+            sentimentPct = 0;
+        }
+
+        bullish = sentimentPct > 0;
+        sentimentPct = Math.abs(sentimentPct);
+
+        if (bullish) {
+            return df.format(sentimentPct) + "% " + "bullish";
+        } else {
+            return df.format(sentimentPct) + "% " + "bearish";
+        }
     }
 }
